d7f80abbb53105f11457072423c6095ac3b9cd51
Irina <shine_202@mail.ru>
2020-05-22 01:03:45 +0300

    change

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -3,19 +3,19 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using System;
+using System.Linq;
 
-    
-    namespace NUnitTestProject1
+namespace NUnitTestProject1
 {
     public class Tests
     {
-        public IWebDriver driver;
+        private IWebDriver driver;
 
         [SetUp]
         public void Setup()
         {
             driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("http://localhost:5000");   
+            driver.Navigate().GoToUrl("http://localhost:5000");
             driver.Manage().Window.Maximize();
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
         }
@@ -25,17 +25,13 @@ using System;
         [Test]
         public void Test1_Autorization()
         {
-            Assert.IsTrue(driver.PageSource.Contains("Login")); // Проверка, что загрузилась нужная страница авторизации с текстом "Login" на ней 
-               
+            Assert.AreEqual("Login", driver.FindElement(By.CssSelector("h2")).Text);  // Проверка загрузки нужной страницы авторизации с текстом "Login" 
+
             driver.FindElement(By.XPath("//input[@id=\"Name\"]")).SendKeys("user");
             driver.FindElement(By.XPath("//input[@id=\"Password\"]")).SendKeys("user");
-
-            Assert.IsTrue(driver.PageSource.Contains("Name")); //Проверка наличия текста "Name" на странице
-            Assert.IsTrue(driver.PageSource.Contains("Password")); //Проверка наличия текста "Password" на странице
-
             driver.FindElement(By.XPath("//input[@type='submit']")).Click();
 
-            Assert.IsTrue(driver.PageSource.Contains("Home page")); // Проверка успешной авторизации - должна быть загружена страница "Home page"
+            Assert.AreEqual("Home page", driver.FindElement(By.CssSelector("h2")).Text); // Проверка успешной авторизации - должна быть загружена страница "Home page"
         }
 
 
@@ -47,22 +43,17 @@ using System;
             // Autorization
             driver.FindElement(By.Id("Name")).SendKeys("user");
             driver.FindElement(By.Id("Password")).SendKeys("user");
-            driver.FindElement(By.XPath("//input[@type='submit']")).Click(); 
-            
-            
-            driver.FindElement(By.XPath("//a[contains(text(), 'All Products')]")).Click(); 
+            driver.FindElement(By.XPath("//input[@type='submit']")).Click();
 
-            Assert.IsTrue(driver.PageSource.Contains("Create new")); // Проверка,что на странице есть  кнопка с текстом "Create new"
 
-            driver.FindElement(By.XPath("//a[contains(text(), 'Create new')]")).Click(); 
+            driver.FindElement(By.XPath("//a[contains(text(), 'All Products')]")).Click();
+            driver.FindElement(By.XPath("//a[contains(text(), 'Create new')]")).Click();
+
+            Assert.AreEqual("Product editing", driver.FindElement(By.CssSelector("h2")).Text); // Проверка загрузки нужной страницы редактирования продукта "Product editing" 
+
             driver.FindElement(By.Id("ProductName")).SendKeys("King prawns");
-
-            SelectElement dropdown1 = new SelectElement(driver.FindElement(By.Id("CategoryId")));
-            dropdown1.SelectByText("Seafood");
-
-            SelectElement dropdown2 = new SelectElement(driver.FindElement(By.Id("SupplierId")));
-            dropdown2.SelectByText("Pavlova, Ltd.");
-                        
+            new SelectElement(driver.FindElement(By.Id("CategoryId"))).SelectByText("Seafood");
+            new SelectElement(driver.FindElement(By.Id("SupplierId"))).SelectByText("Pavlova, Ltd.");
             driver.FindElement(By.Id("UnitPrice")).SendKeys("500");
             driver.FindElement(By.Id("QuantityPerUnit")).SendKeys("24 pieces");
             driver.FindElement(By.Id("UnitsInStock")).SendKeys("20");
@@ -70,13 +61,9 @@ using System;
             driver.FindElement(By.Id("ReorderLevel")).SendKeys("2");
             driver.FindElement(By.Id("Discontinued")).Click();
 
-            Assert.IsTrue(driver.FindElement(By.XPath("//input[@id=\"Discontinued\"]")).Enabled);// проверка,включен ли элемент "Discontinued"
-            Assert.IsTrue(driver.PageSource.Contains("Product editing")); // Проверка, что загрузилась нужная страница редакт-я продукта  "Product editing" 
+            Assert.IsTrue(driver.FindElement(By.XPath("//input[@id=\"Discontinued\"]")).Enabled);// проверка, включен ли элемент "Discontinued"
 
             driver.FindElement(By.XPath("//input[@type=\"submit\"]")).Click();
-
-            Assert.IsTrue(driver.PageSource.Contains("King prawns")); // Проверка,что продукт создан - название есть на странице
-            
         }
 
 
@@ -89,7 +76,7 @@ using System;
             driver.FindElement(By.Id("Password")).SendKeys("user");
             driver.FindElement(By.XPath("//input[@type='submit']")).Click();
 
-            driver.FindElement(By.XPath("//a[contains(text(), 'All Products')]")).Click(); 
+            driver.FindElement(By.XPath("//a[contains(text(), 'All Products')]")).Click();
             Assert.AreEqual("King prawns", driver.FindElement(By.XPath("//a[text()=\"King prawns\"]")).Text); // Проверка ProductName
             Assert.AreEqual("Seafood", driver.FindElement(By.XPath("(//td/a[text()=\"King prawns\"]/..//following-sibling::td[1])")).Text); // CategoryName
             Assert.AreEqual("Pavlova, Ltd.", driver.FindElement(By.XPath("(//td/a[text()=\"King prawns\"]/..//following-sibling::td[2])")).Text); // Проверка SupplierName
@@ -99,15 +86,8 @@ using System;
             Assert.AreEqual("3", driver.FindElement(By.XPath("(//td/a[text()=\"King prawns\"]/..//following-sibling::td[6])")).Text); // Проверка UnitsOnOrder
             Assert.AreEqual("2", driver.FindElement(By.XPath("(//td/a[text()=\"King prawns\"]/..//following-sibling::td[7])")).Text); // Проверка ReorderLevel
             Assert.AreEqual("True", driver.FindElement(By.XPath("(//td/a[text()=\"King prawns\"]/..//following-sibling::td[8])")).Text); // Проверка Discontinued
-
-
-            
-            Assert.IsTrue(driver.PageSource.Contains("ProductName")); // Проверка,что на странице есть "ProductName"
-            Assert.IsTrue(driver.PageSource.Contains("King prawns")); // Проверка,что на странице есть "ProductId"
-            Assert.IsTrue(driver.PageSource.Contains("CategoryName")); // Проверка,что на странице есть "CategoryName"
-
-
         }
+
 
         //Test 4. Delete Created Products
         [Test]
@@ -119,17 +99,15 @@ using System;
             driver.FindElement(By.XPath("//input[@type='submit']")).Click();
 
 
-            driver.FindElement(By.XPath("//a[contains(text(), 'All Products')]")).Click(); // Переходим в меню "Продукты"
+            driver.FindElement(By.XPath("//a[contains(text(), 'All Products')]")).Click();
             driver.FindElement(By.XPath("(//td/a[text()=\"King prawns\"]/..//following-sibling::td[10])")).Click(); // нажимаем ""Удалить"
-            driver.SwitchTo().Alert().Accept();
-
-            Assert.IsTrue(driver.PageSource.Contains("King prawns"));  // Проверка, действительно ли продукт удален (присутствует ли он в списке)
+            driver.SwitchTo().Alert().Accept(); //Подтверждение удаления в всплывающем окне предупреждения
 
         }
 
 
         //Test 5. Logout
-                [Test]
+        [Test]
         public void Test5_Logout()
         {
 
@@ -140,20 +118,14 @@ using System;
             driver.FindElement(By.XPath("//input[@type='submit']")).Click();
 
 
-            Assert.IsTrue(driver.PageSource.Contains("Logout")); //Проверка наличия текста ссылки "Logout" на странице
-            
             driver.FindElement(By.XPath("//a[text()='Logout']")).Click(); //LOGOUT
             AssertionException.Equals("user", driver.FindElement(By.Id("Name"))); // проверка,что поле не содержит ранее введенный текст "user"
             AssertionException.Equals("user", driver.FindElement(By.Id("Password"))); // проверка,что поле не содержит ранее введенный текст "user"
 
-
             driver.FindElement(By.Id("Name")).SendKeys("user");
             driver.FindElement(By.Id("Password")).SendKeys("user");
-            
-
             driver.FindElement(By.XPath("//input[@type='submit']")).Click();
         }
-
 
 
 
@@ -163,7 +135,7 @@ using System;
         {
             driver.Close();
             driver.Quit();
-}
+        }
     }
 
 }
